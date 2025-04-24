12345bae4504e4c42e849c4731e0d6ade41de98a
Ealvn <39033468+Ealvn@users.noreply.github.com>
2025-04-25 01:33:53 +0200

    Update KemonoTrans.js

--- KemonoTrans.js
+++ KemonoTrans.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name            Kemono跳转-e
 // @description     在一些特定网站添加跳转至kemono按钮
+// @version         1.0.0
 // @author          Ealvn
 // @license         MIT
 // @match           https://*.pixiv.net/*
